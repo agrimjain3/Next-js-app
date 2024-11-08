fa0d3011c5bda940099be2f2bc4949d4bb95adcd
Agrim Jain <agrim.jain@altudo.co>
2024-11-08 14:42:03 +0530

    chg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+    assetPrefix: './',
+    basePath: '/Next-js-app', // Change this to your repository name
+  };
+  
