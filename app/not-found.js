c3f3e98f10b4a500fef8985ff0ec88e9d1c9b4b6
Agrim Jain <agrim.jain@altudo.co>
2024-11-08 12:50:07 +0530

    adding final project

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <>
+      <main>
+        <h1>MEAL NOT FOUND</h1>
+        <p> Maybe try another meal, Thanks!</p>
+      </main>
+    </>
+  );
+}
